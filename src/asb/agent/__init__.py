056148912c49a82f308e5e13324d8a3eb865d23b
Morteza Mirjebreili <34815142+mirjebreili@users.noreply.github.com>
2025-09-22 08:37:12 +0330

    Add state schema generator and tests

--- src/asb/agent/__init__.py
+++ src/asb/agent/__init__.py
@@ -2,10 +2,13 @@
 
 from .architecture_designer import architecture_designer_node, design_architecture
 from .requirements_analyzer import analyze_requirements, requirements_analyzer_node
+from .state_generator import generate_state_schema, state_generator_node
 
 __all__ = [
     "analyze_requirements",
     "architecture_designer_node",
     "design_architecture",
+    "generate_state_schema",
     "requirements_analyzer_node",
+    "state_generator_node",
 ]
